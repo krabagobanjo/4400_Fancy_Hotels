f00ece6bdb2103ebddebe22e1169257cbaa876c3
Kyle Rabago-Banjo <krabagobanjo@outlook.com>
2015-11-10 15:25:08 -0500

    Changed frame handling, no longer needs a list

--- src/FH_presenter.py
+++ src/FH_presenter.py
@@ -9,23 +9,21 @@ class FH_presenter(Tk):
         # the container is where we'll stack a bunch of frames
         # on top of each other, then the one we want visible
         # will be raised above the others
-        container = Frame(self)
-        container.pack(side="top", fill="both", expand=True)
-        container.grid_rowconfigure(0, weight=1)
-        container.grid_columnconfigure(0, weight=1)
+        self.container = Frame(self)
+        self.container.pack(side="top", fill="both", expand=True)
+        self.container.grid_rowconfigure(0, weight=1)
+        self.container.grid_columnconfigure(0, weight=1)
 
-        self.frames = {}
-        for F in (StartPage, PageOne, PageTwo):
-            frame = F(container, self)
-            self.frames[F] = frame
-            # put all of the pages in the same location;
-            # the one on the top of the stacking order
-            # will be the one that is visible.
-            frame.grid(row=0, column=0, sticky="nsew")
 
-        self.show_frame(StartPage)
-
-    def show_frame(self, c):
-        '''Show a frame for the given class'''
-        frame = self.frames[c]
+        frame = StartPage(self.container, self)
+        frame.grid(row=0, column=0, sticky="nsew")
+        self.curr_frame = frame
         frame.tkraise()
+
+    def show_frame(self, callee):
+        '''Show a frame for the given class'''
+        frame = callee(self.container, self)
+        frame.grid(row=0, column=0, sticky="nsew")
+        frame.tkraise()
+        self.curr_frame.destroy()
+        self.curr_frame = frame
